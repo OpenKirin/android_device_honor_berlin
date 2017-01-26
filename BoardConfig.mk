aba78ba8309050b3c0c7397d0d68ff8d4549d4d9
Surdu Petru <surdu_petru@yahoo.com>
2017-01-26 09:52:47 +0100

    remove unneeded wifi flags

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -133,19 +133,8 @@ BOARD_RIL_CLASS := ../../../device/honor/berlin/ril
 ENABLE_WEBGL := true
 
 # Wifi
-TARGET_USES_64_BIT_BCMDHD	 := true
-BOARD_WLAN_DEVICE                := bcmdhd
-BOARD_WLAN_DEVICE_REV            := bcm4345
 WPA_SUPPLICANT_VERSION           := VER_0_8_X
-BOARD_WPA_SUPPLICANT_DRIVER      := NL80211
-BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_bcmdhd
 BOARD_HOSTAPD_DRIVER             := NL80211
-BOARD_HOSTAPD_PRIVATE_LIB        := lib_driver_cmd_bcmdhd
-WIFI_DRIVER_FW_PATH_PARAM        := "/sys/module/bcmdhd/parameters/firmware_path"
-WIFI_DRIVER_FW_PATH_STA          := "/system/vendor/firmware/fw_bcm43455_hw.bin"
-WIFI_DRIVER_FW_PATH_AP           := "/system/vendor/firmware/fw_bcm43455_apsta_hw.bin"
-WIFI_DRIVER_FW_PATH_P2P          := "/system/vendor/firmware/fw_bcm43455_hw.bin"
-WIFI_BAND                        := 802_11_ABG
 
 # inherit from the proprietary version
 -include vendor/honor/berlin/BoardConfigVendor.mk
