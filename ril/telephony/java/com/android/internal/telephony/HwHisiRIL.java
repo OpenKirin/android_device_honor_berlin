2a423632505d1969967490afdd617f0d278cdbbd
Surdu Petru <surdu_petru@yahoo.com>
2017-01-26 15:55:17 +0100

    keep LineageOS Copyright

--- ril/telephony/java/com/android/internal/telephony/HwHisiRIL.java
+++ ril/telephony/java/com/android/internal/telephony/HwHisiRIL.java
@@ -1,8 +1,8 @@
 /*
- * Copyright (c) 2012-2013, The Linux Foundation. All rights reserved.
+ * Copyright (c) 2012-2017, The Linux Foundation. All rights reserved.
  * Not a Contribution.
  *
- * Copyright (C) 2006 The Android Open Source Project
+ * Copyright (C) 2017 The LineageOS Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
